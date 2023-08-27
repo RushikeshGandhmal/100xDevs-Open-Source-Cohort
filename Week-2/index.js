2d8f27976f32bfc0bba3fe7c0dce7ef4d479a895
Rushi <rushikeshgandhmal@gmail.com>
2023-08-27 23:24:04 +0530

    express

---
+++ Week-2/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const prompt = require("prompt-sync")();
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const number = Number(prompt("enter a number"));
+  const sum = calculateSum(number);
+  res.send(`Calculated sum is ${sum}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app is listening on ${port}`);
+});
+
+const calculateSum = (n) => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+};
